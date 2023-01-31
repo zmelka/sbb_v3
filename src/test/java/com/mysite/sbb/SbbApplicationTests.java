500d9ab6ce040695bddf1cf1a7afce229ec9dde7
zemlka <jjdlwodud@naver.com>
2023-01-31 22:06:34 +0900

    findAll

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -12,16 +16,27 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
+
+
 	@Test
 	void testJpa() {
+		List<Question> all = questionRepository.findAll();
+		assertEquals(2,all.size());
+
+		Question q= all.get(0);
+		assertEquals("sbb가 무엇인가요?",q.getSubject());
+	}
+
+	@Test
+	void testCreateQuestions() {
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
 		q1.setCreateDate(LocalDateTime.now());
 		questionRepository.save(q1);
 
 		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다");
+		q2.setSubject("스프링부트 모델 질문입니다.");
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
 		questionRepository.save(q2);
