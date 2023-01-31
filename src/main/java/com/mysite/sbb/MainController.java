912ba26c1f5c13d38db19bb6ddef8ed5dc139b1d
zemlka <jjdlwodud@naver.com>
2023-01-31 17:04:47 +0900

    세팅

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -11,6 +11,6 @@ public class MainController {
     @GetMapping("/sbb")
     @ResponseBody
     public String index() {
-        return "안녕하세요 sbb에 오신것을 환영합니다.";
+        return "안녕 sbb에 오신것을 환영합니다..";
     }
 }
