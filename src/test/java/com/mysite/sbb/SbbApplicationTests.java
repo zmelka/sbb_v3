0a0791023bf35a523198aefc81e9eee814fcfe4d
zemlka <jjdlwodud@naver.com>
2023-02-02 16:23:37 +0900

    데이터 삭제

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -8,8 +8,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -17,6 +16,22 @@ class SbbApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
+
+	@Test
+	void testRemoveQuestino() {
+		assertEquals(2,questionRepository.count());
+		Question q =questionRepository.findBySubject("sbb가 무엇인가요?");
+		questionRepository.delete(q);
+		assertEquals(1,questionRepository.count());
+
+
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);
+	}
+
 
 	@Test
 	void testModifyQuestinoSubject() {
