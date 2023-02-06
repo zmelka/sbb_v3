f3ac562babbc36e515f23da4c7b6d1716a30b384
zemlka <jjdlwodud@naver.com>
2023-02-06 22:09:23 +0900

    서비스 적용

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -11,12 +11,13 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
+
     @Autowired
-    private QuestionRepository questionRepository;
+    private QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model){
-        List<Question> questionList = questionRepository.findAll();
+        List<Question> questionList = questionService.getList();
         model.addAttribute("questionList",questionList);
 
         return "question_list";
---
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -0,0 +1,19 @@
+package com.mysite.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList(){
+        return questionRepository.findAll();
+    }
+
+}
